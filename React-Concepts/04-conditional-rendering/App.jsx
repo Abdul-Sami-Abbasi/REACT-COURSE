9a8782377c0e0eeebab7fe27e5781ad5d7b9e770
Abdul Sami Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2025-07-08 14:56:32 +0500

    Add files via upload

---
+++ React-Concepts/04-conditional-rendering/App.jsx
@@ -0,0 +1,34 @@
+// File: 04-conditional-rendering/App.jsx
+
+import React from "react";
+
+// Conditional Component
+function Greeting({ isLoggedIn }) {
+  if (isLoggedIn) {
+    return <h2>Welcome, User!</h2>;
+  } else {
+    return <h2>Please Log In</h2>;
+  }
+}
+
+function App() {
+  const isLoggedIn = true;
+  const hasPermission = true;
+
+  return (
+    <div>
+      <h1>Conditional Rendering Examples</h1>
+
+      {/* Conditional return component */}
+      <Greeting isLoggedIn={isLoggedIn} />
+
+      {/* Ternary operator */}
+      <p>{isLoggedIn ? "You are logged in." : "You are logged out."}</p>
+
+      {/* Logical && operator */}
+      {hasPermission && <p>You can view this content.</p>}
+    </div>
+  );
+}
+
+export default App;
