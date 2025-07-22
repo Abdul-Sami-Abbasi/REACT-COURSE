be155a347acfee6ffa46479b1e102aa51b2a2e35
Abdul Sami Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2025-07-22 09:11:19 +0500

    Add files via upload

---
+++ React-Concepts/14-useId-hook/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FormInput from "./FormInput";
+
+function App() {
+  return (
+    <div>
+      <h2>useId Hook Example</h2>
+      <FormInput />
+      <FormInput />
+    </div>
+  );
+}
+
+export default App;
